2dbac390e29601932f64bedcd5272e941a9ca373
Benjamin Rood <benisrood@gmail.com>
2015-05-10 15:06:38 +1200

    Initial Commit

---
+++ UTM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UTM
+//
+//  Created by Benjamin Rood on 10/05/15.
+//  Copyright (c) 2015 bisr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
